a4c2142888e6fc67b13cd6f90eccefb79d6112d0
SAStefanov <SAStefanov@ixora-auto.ru>
2022-09-06 16:50:00 +0300

    initial changes with webpack

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import _ from 'lodash';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import printMe from './print.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
